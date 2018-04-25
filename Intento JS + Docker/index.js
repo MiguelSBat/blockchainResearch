14cf96eca8eb4fae6e72d1c34278150e4227e477
root <root@Idun>
2018-04-25 20:47:41 +0200

    On branch alt 	modified:   Codigos IPFS.txt 	modified:   Dockerfile 	modified:   index.js 	new file:   package.json

--- Intento JS + Docker/index.js
+++ Intento JS + Docker/index.js
@@ -2,15 +2,13 @@ const request = require("request");
 const url =
   "https://maps.googleapis.com/maps/api/geocode/json?address=Florence";
 
-var before = Date.now();
-
-request.get(url, (error, response, body) => {
+request.get(url,{time:true}, (error, response, body) => {
   let json = JSON.parse(body);
-  var after = Date.now();
-  var time = after - before;
 
   console.log(
-    `Status: ${json.status} | Time: ${time}`
+    `Status: ${json.status} | Time: ${response.elapsedTime}`
   );
+   process.exit(0);
+  // With this line we can get more details about the request
+ // console.log(request.timingPhases);
 });
-
