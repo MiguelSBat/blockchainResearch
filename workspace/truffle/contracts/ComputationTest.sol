f31c00647114fbac26b8c75335ed4f09a2b73205
root <root@Idun>
2018-05-09 20:36:41 +0200

    modified:   workspace/truffle/contracts/ComputationTest.sol

--- workspace/truffle/contracts/ComputationTest.sol
+++ workspace/truffle/contracts/ComputationTest.sol
@@ -1,8 +1,9 @@
 
 /*
-   Oraclize computationç
-   Currently working
+   Oraclize computation
+https://rinkeby.etherscan.io/address/0x5aed47556404592e5a29a816538e0957de9caeea
 */
+
 
 
 pragma solidity ^0.4.2;
@@ -11,24 +12,37 @@ import "github.com/oraclize/ethereum-api/oraclizeAPI.sol";
 
 contract ComputationJson is usingOraclize {
     
-    string public lastCallback;
+    uint public lastCallback;
+    uint public maxTimeResponse;
+    mapping(uint => string) public SLAViolations;
     
-    event newCallBackTest(string _result);
+    event newCallBackTest(string _result, uint _blockNumber);
+    event newSLAViolation(string _message,uint _responseTime, uint _timeStamp,uint _blockNumber);
     
     function ComputationJson() {
         oraclize_setProof(proofType_TLSNotary | proofStorage_IPFS); 
+        maxTimeResponse = 200;
         callingDocker();
     }
 
     function __callback(bytes32 myid, string result, bytes proof) {
         if (msg.sender != oraclize_cbAddress()) throw;
-        lastCallback = result;
-       newCallBackTest(lastCallback);
+        lastCallback = parseInt(result);
+        newCallBackTest(result, block.number);
+        if(lastCallback>maxTimeResponse){
+            SLAViolations[block.number] = "Max time response exceeded!";
+            newSLAViolation("Max time response exceeded!",lastCallback,now,block.number);
+        }
+        
     }
     
     function  callingDocker() payable {
-
-        oraclize_query("computation",["json(Qmb7E5ZyaQzhUcqNVTQKJjWQzVGTm6zWQ7FPQuJYMBjCA6).[http-status,response-time]"]);
+        /*
+        oraclize_query("computation",["json(Qmb7E5ZyaQzhUcqNVTQKJjWQzVGTm6zWQ7FPQuJYMBjCA6).[http-status,response-time]"]); 
+        This query return an array-like string, I dont know how to parse it.        EX: '[200, 429]'
+        */
+        
+        oraclize_query("computation",["json(Qmb7E5ZyaQzhUcqNVTQKJjWQzVGTm6zWQ7FPQuJYMBjCA6).response-time"]);
     }
     
     
